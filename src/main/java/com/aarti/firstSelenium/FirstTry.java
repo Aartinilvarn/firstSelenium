6281ec9a88e931e52e86cd03d020ea35d3ce32b0
Aartinilvarn <aartinilvarn5@gmail.com>
2023-10-08 23:44:09 +0530

    initial commit

---
+++ src/main/java/com/aarti/firstSelenium/FirstTry.java
@@ -0,0 +1,25 @@
+package com.aarti.firstSelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.webdriver.WebDriverBrowser;
+
+public class FirstTry {
+	public static void main(String args[]) throws InterruptedException {
+		EdgeDriver dr=new EdgeDriver();
+		
+//		dr.get("https://www.google.com");
+		dr.get("https://www.w3schools.com/quiztest/quiztest.asp?qtest=HTML");
+		dr.manage().window().maximize();
+		
+//		Thread.sleep(5000);
+		dr.findElement(By.id("3")).click();
+		dr.findElement(By.xpath("//*[@id=\"answerbuttoncontainer\"]/button")).click();
+		
+//		dr.findElement(By.xpath("//*[@id=\"APjFqb\"]")).sendKeys("iphone 15");
+//		dr.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]")).click();
+	}
+
+}
---
+++ src/test/java/com/aarti/firstSelenium/FirstSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarti.firstSelenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
